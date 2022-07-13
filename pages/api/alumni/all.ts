480e9f5794d45a7c5fe02295086b086a2127aa25
Semet <hamdanilombok@gmail.com>
2022-07-13 17:13:02 +0800

    add alumni api

---
+++ pages/api/alumni/all.ts
@@ -0,0 +1,45 @@
+import { StudentWithDetails } from "common";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../prisma/db";
+
+type Data = {
+	students?: StudentWithDetails[];
+	error?: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+	try {
+		const { department, year } = req.query;
+		const dep = await prisma.department.findUnique({
+			where: {
+				id: Number(department),
+			},
+			include: {
+				batches: {
+					include: {
+						classRooms: {
+							include: {
+								students: {
+									where: {
+										yearId: Number(year),
+										// isAlumni: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		});
+		const data = dep?.batches.flatMap((batch) =>
+			batch.classRooms.flatMap((classRoom) => classRoom.students)
+		);
+		console.log(data);
+		res.status(200).json({ students: data });
+		res.end();
+	} catch (err) {
+		res.status(500).json({ error: "Unable to fetch students" });
+		res.end();
+	}
+}
