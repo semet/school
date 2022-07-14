f0a163cd62ecd6b4d1d1e8be9448d9c4ce0a3b0f
Semet <hamdanilombok@gmail.com>
2022-07-14 21:41:19 +0800

    clea up

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-	name: string;
-};
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	res.status(200).json({ name: "John Doe" });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+	try {
+	} catch (err) {}
 }
