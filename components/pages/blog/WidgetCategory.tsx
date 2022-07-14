3bbd2d36ea0f788047b6f803387f14c0ba2b7e56
Semet <hamdanilombok@gmail.com>
2022-07-14 21:41:09 +0800

    get content from api

--- components/pages/blog/WidgetCategory.tsx
+++ components/pages/blog/WidgetCategory.tsx
@@ -1,68 +1,41 @@
+import { CategoryWithDetails } from "common";
+import Link from "next/link";
 import React from "react";
+import { useQuery } from "react-query";
 
 const WidgetCategory = () => {
+	const { data, isFetching } = useQuery<CategoryWithDetails[]>(
+		"categories",
+		async () => {
+			// await new Promise((res) => setTimeout(res, 1000));
+			const res = await fetch("/api/category/all");
+			const data = await res.json();
+
+			return data.categories;
+		}
+	);
+
 	return (
 		<div className="widget widget-category">
 			<div className="widget-header">
 				<h5>Post Categories</h5>
 			</div>
 			<ul className="lab-ul widget-wrapper list-bg-none">
-				<li>
-					<a href="#" className="d-flex flex-wrap justify-content-between">
-						<span>
-							<i className="icofont-rounded-double-right"></i>Show all
-						</span>
-						<span>18</span>
-					</a>
-				</li>
-				<li>
-					<a href="#" className="d-flex flex-wrap justify-content-between">
-						<span>
-							<i className="icofont-rounded-double-right"></i>Business
-						</span>
-						<span>20</span>
-					</a>
-				</li>
-				<li>
-					<a href="#" className="d-flex flex-wrap justify-content-between">
-						<span>
-							<i className="icofont-rounded-double-right"></i>Creative
-						</span>
-						<span>25</span>
-					</a>
-				</li>
-				<li>
-					<a href="#" className="d-flex flex-wrap justify-content-between">
-						<span>
-							<i className="icofont-rounded-double-right"></i>Inspiation
-						</span>
-						<span>30</span>
-					</a>
-				</li>
-				<li>
-					<a href="#" className="d-flex flex-wrap justify-content-between">
-						<span>
-							<i className="icofont-rounded-double-right"></i>News
-						</span>
-						<span>28</span>
-					</a>
-				</li>
-				<li>
-					<a href="#" className="d-flex flex-wrap justify-content-between">
-						<span>
-							<i className="icofont-rounded-double-right"></i>Photography
-						</span>
-						<span>20</span>
-					</a>
-				</li>
-				<li>
-					<a href="#" className="d-flex flex-wrap justify-content-between">
-						<span>
-							<i className="icofont-rounded-double-right"></i>Smart
-						</span>
-						<span>26</span>
-					</a>
-				</li>
+				{data !== undefined
+					? data.map((category) => (
+							<li key={category.id}>
+								<Link href={`/category/${category.slug}`}>
+									<a className="d-flex flex-wrap justify-content-between">
+										<span>
+											<i className="icofont-rounded-double-right"></i>
+											{category.name}
+										</span>
+										<span>{category._count.posts}</span>
+									</a>
+								</Link>
+							</li>
+					  ))
+					: null}
 			</ul>
 		</div>
 	);
