437d7ba8e8af7f5f35d31760349f5e82290032a4
Semet <hamdanilombok@gmail.com>
2022-07-16 23:13:08 +0800

    clean up

--- prisma/migrations/20220716131330_/migration.sql
+++ prisma/migrations/20220716131330_/migration.sql
@@ -338,6 +338,7 @@ CREATE TABLE `GalleryImage` (
     `id` VARCHAR(191) NOT NULL,
     `galleryId` VARCHAR(191) NOT NULL,
     `caption` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
