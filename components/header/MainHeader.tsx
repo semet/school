8c20575d03aa98d32a52ad859607da21981ae0a6
Semet <hamdanilombok@gmail.com>
2022-07-16 23:10:36 +0800

    add alert for PSB

--- components/header/MainHeader.tsx
+++ components/header/MainHeader.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React from "react";
 import HeaderTop from "./HeaderTop";
 import MainMenu from "./MainMenu";
@@ -6,6 +7,13 @@ import MobileMenu from "./MobileMenu";
 const MainHeader = () => {
 	return (
 		<header className="header-3 pattern-1">
+			{/* <div className="alert alert-info text-center">
+				<strong>Penerimaan siswa baru sudah dibuka!</strong> Klik{" "}
+				<a href="" className="text-danger">
+					link ini
+				</a>{" "}
+				untuk mendaftar
+			</div> */}
 			<div className="container">
 				<div className="row align-items-center justify-content-center">
 					<div className="col-xl-3 col-12">
