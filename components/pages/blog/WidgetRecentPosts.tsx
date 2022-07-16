e66613ac6289167c7c399e251628e6a1a803727a
Semet <hamdanilombok@gmail.com>
2022-07-16 17:59:19 +0800

    optimize recent post

--- components/pages/blog/WidgetRecentPosts.tsx
+++ components/pages/blog/WidgetRecentPosts.tsx
@@ -1,65 +1,49 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import { PostWithDetails } from "common";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import formatDate from "dateformat";
 
 const WidgetRecentPosts = () => {
+	const { data, isFetching, isLoading } = useQuery<PostWithDetails[]>(
+		"recentPosts",
+		async () => {
+			// await new Promise((res) => setTimeout(res, 1000));
+			const res = await fetch("/api/post/recent");
+			const data = await res.json();
+
+			return data.posts;
+		}
+	);
+
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
 	return (
 		<div className="widget widget-post">
 			<div className="widget-header">
 				<h5>Recent Post</h5>
 			</div>
 			<ul className="lab-ul widget-wrapper">
-				<li className="d-flex flex-wrap justify-content-between">
-					<div className="post-thumb">
-						<a href="blog-single.html">
-							<img src="/images/product/01.jpg" alt="product" />
-						</a>
-					</div>
-					<div className="post-content ps-4">
-						<a href="blog-single.html">
-							<h6>Poor People’s Campaign Our Resources</h6>
-						</a>
-						<p>04 February 2019</p>
-					</div>
-				</li>
-				<li className="d-flex flex-wrap justify-content-between">
-					<div className="post-thumb">
-						<a href="blog-single.html">
-							<img src="/images/product/02.jpg" alt="product" />
-						</a>
-					</div>
-					<div className="post-content ps-4">
-						<a href="blog-single.html">
-							<h6>Boosting Social For NGO And Charities </h6>
-						</a>
-						<p>04 February 2019</p>
-					</div>
-				</li>
-				<li className="d-flex flex-wrap justify-content-between">
-					<div className="post-thumb">
-						<a href="blog-single.html">
-							<img src="/images/product/03.jpg" alt="product" />
-						</a>
-					</div>
-					<div className="post-content ps-4">
-						<a href="blog-single.html">
-							<h6>Poor People’s Campaign Our Resources</h6>
-						</a>
-						<p>04 February 2019</p>
-					</div>
-				</li>
-				<li className="d-flex flex-wrap justify-content-between">
-					<div className="post-thumb">
-						<a href="blog-single.html">
-							<img src="/images/product/04.jpg" alt="product" />
-						</a>
-					</div>
-					<div className="post-content ps-4">
-						<a href="blog-single.html">
-							<h6>Boosting Social For NGO And Charities </h6>
-						</a>
-						<p>04 February 2019</p>
-					</div>
-				</li>
+				{isFetching || isLoading ? (
+					<p>Loading . . .</p>
+				) : data !== undefined ? (
+					data.map((post) => (
+						<li className="d-flex flex-wrap justify-content-between" key={post.id}>
+							<div className="post-thumb">
+								<a href="blog-single.html">
+									<img src="/images/product/01.jpg" alt="product" />
+								</a>
+							</div>
+							<div className="post-content ps-4">
+								<a href="blog-single.html">
+									<h6>{post.title}</h6>
+								</a>
+								<p>{formatDate(post.createdAt, "fullDate")}</p>
+							</div>
+						</li>
+					))
+				) : null}
 			</ul>
 		</div>
 	);
